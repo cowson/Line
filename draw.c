bb95915e86fc45ad57340c79f6b6c52380a56a2e
cowson <nielsgraham@gmail.com>
2017-02-16 08:23:35 -0500

    code

--- draw.c
+++ draw.c
@@ -7,19 +7,68 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  int x, y, d;
-  int A = y1 - y0;
-  int B = -(x1-x0);
+  int x, y, A, B, d;
+	if (x0 - x1 != 0){
+		float m = ((y0 - y1)/(x0 - x1));
+  	if (m  < 0){ //reverse line if it is going wrong way
+    	x = x0;
+    	y = y0;
+    	x0 = x1;
+    	y0 = y1;
+    	x1 = x;
+    	y1 = y;
+  	}
+}
   x = x0;
   y = y0;
-  d = 2*A + B;
-  while(x <= x1) {
-    plot(s, c, x, y);
-    if (d > 0) {
-      y++;
-      d+=(2*B);
+  A = y1 - y0;
+  B = -1 * (x1 - x0);
+  if ((B * -1) >= A && A >= 0){//octant 1
+    d = 2 * A + B;
+    while (x < x1){
+      plot(s, c, x, y);
+      if (d > 0){
+	y++;
+	d += 2 * B;
+      }
+      x++;
+      d += 2 * A;
     }
-    x++;
-    d+= 2*A;
+  }
+  else if (A > (B * -1) && A >= 0){//octant 2
+    d = A + 2 * B;
+    while (y < y1){
+      plot(s, c, x, y);
+	if (d < 0){
+	  x++;
+	  d += 2 * A;
+	}
+      y++;
+      d += 2 * B;
+    }
+  }
+  else if (A < B){//octant 3
+    d = A - 2 * B;
+    while (y > y1){
+      plot(s, c, x, y);
+      if (d > 0){
+	x++;
+	d += 2 * A;
+      }
+      y--;
+      d -= 2 * B;
+    }
+  }
+  else {//octant 4
+    d = 2 * A - B;
+    while (x < x1){
+      plot(s, c, x, y);
+      if (d < 0){
+	y--;
+	d -= 2 * B;
+      }
+      x++;
+      d += 2 * A;
+    }
   }
 }
--- main.c
+++ main.c
@@ -13,13 +13,54 @@ int main() {
 
 
   c.red = 0;
-  c.green = MAX_COLOR;
+  c.green =0;
   c.blue = 0;
 
   clear_screen(s);
+int i,k,l,m;
+ i = 0;
+ k = 0;
+ l = 0;
+ m = 0;
+ for(i;i<500;i++){
+    if (i > 250)
+      c.red = i - 250;
+     c.blue = i;
+     draw_line(250, 250, 500, 500 - i,s,c);
+ }
+ for(l;l<500;l++){
+     c.green = l;
+     if (l > 250)
+      c.red= 250 - (l - 250);
 
-  draw_line( 0,0, 500,500, s,c );
-  draw_line( 0,4, 500,90, s,c);
+     draw_line(250 ,250 , 500 - l, 0 ,s,c);
+ }
+ for(k;k<500;k++){
+   if (k < 250)
+     c.blue = 250 - k;
+   if (k > 250)
+    c.green = 250 - (k - 250);
+     //if (k > 250)
+       //c.green = 250 - (k - 250);
+     draw_line(0 , k, 250,250 ,s,c);
+ }
+ for(m;m<500;m++){
+   if (m < 250){
+     c.red = m;
+     c.green =m;
+     c.blue = m;
+   }
+   else{
+     c.red = 250- (m- 250);
+     c.green =250 - (m - 250);
+     c.blue = 250 - (m- 250);
+   }
+     draw_line(m ,500 ,250,250 ,s,c);
+ }
+
+
+
+
   display(s);
   save_extension(s, "lines.png");
 }
